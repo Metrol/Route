37dbc3873b60e28ca0b03f5f5067c1657ce26405
Michael Collette <metrol@metrol.net>
2016-12-12 16:57:06 -0600

    Add layers to calling on controller actions to insure they exist
    Need some additional steps to catch when a route or controller action
    isn't found so the caller can take the appropriate actions.  Likely,
    call on a 404 page to be displayed.
    
    This is a major API change, as the caller now has to call run(), then
    request a status, then finally execute() if all is good to go.

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -7,7 +7,6 @@
  */
 
 namespace Metrol;
-use Metrol\Request;
 
 /**
  * Converts a Route into the calls to action
@@ -16,6 +15,15 @@ use Metrol\Request;
 class Dispatcher
 {
     /**
+     * Errors that may occur when trying to get a route
+     *
+     * @const integer
+     */
+    const READY_TO_EXECUTE     = 0;
+    const ROUTE_NOT_FOUND      = 12;
+    const ACTION_NOT_FOUND     = 25;
+
+    /**
      * The Request object passed into the constructor that will be used to
      * determine which route to run actions from, and will be passed into
      * the actions run.
@@ -23,6 +31,28 @@ class Dispatcher
      * @var Request
      */
     protected $request;
+
+    /**
+     * The status of the route request following a run.
+     *
+     * @var integer|null
+     */
+    protected $routeStatus = null;
+
+    /**
+     * Set of found actions to be executed
+     *
+     * @var Route\Action[]
+     */
+    protected $actions = [];
+
+    /**
+     * The argument values found in the request to be passed along to the
+     * action.
+     *
+     * @var array
+     */
+    protected $arguments = [];
 
     /**
      * Takes in and saves the Route that will be loaded up along with the
@@ -36,38 +66,110 @@ class Dispatcher
     }
 
     /**
-     * Executes the actions found in the route, then outputs the last action
-     * back to the caller.
+     * Performs a look up on the request to establish all the actions that need
+     * to be executed.  This will also set the route status flag accordingly.
      *
-     * @return string
+     * @return $this
      */
     public function run()
     {
-        $out = '';
-
         $route = $this->findRoute();
 
         if ( $route == null )
         {
-            return $out;
+            $this->routeStatus = self::ROUTE_NOT_FOUND;
+
+            return $this;
         }
 
-        $actions   = $route->getActions();
-        $arguments = $route->getArguments();
+        $this->actions   = $route->getActions();
+        $this->arguments = $route->getArguments();
 
-        foreach ( $actions as $action )
+        $this->verifyActions();
+
+        return $this;
+    }
+
+    /**
+     * Provide the route status following run() being called
+     *
+     * @return integer|null
+     */
+    public function getRouteStatus()
+    {
+        return $this->routeStatus;
+    }
+
+    /**
+     * Make sure all the controllers and actions in the route exist and are
+     * ready to be run.  Set the route status accordingly.
+     *
+     * @return void
+     */
+    protected function verifyActions()
+    {
+        foreach ( $this->actions as $action )
         {
             if ( ! $action->isReady() )
             {
-                continue;
+                $this->routeStatus = self::ACTION_NOT_FOUND;
+
+                return;
             }
 
+            $controllerClass = $action->getControllerClass();
+
+            if ( ! class_exists($controllerClass) )
+            {
+                $this->routeStatus = self::ACTION_NOT_FOUND;
+
+                return;
+            }
+
+            $method = $action->getControllerMethod();
+
+            if ( ! method_exists($controllerClass, $method) )
+            {
+                $this->routeStatus = self::ACTION_NOT_FOUND;
+
+                return;
+            }
+        }
+
+        // Route was found, and all the actions are ready to go.
+        $this->routeStatus = self::READY_TO_EXECUTE;
+    }
+
+    /**
+     * Execute the action based on the found route following a run.
+     *
+     * @return string
+     *
+     * @throws \UnderflowException
+     * @throws \OutOfBoundsException
+     */
+    public function execute()
+    {
+        if ( $this->routeStatus === null )
+        {
+            throw new \UnderflowException('Can not execute a controller action without run() first');
+        }
+
+        if ( $this->routeStatus !== self::READY_TO_EXECUTE )
+        {
+            throw new \OutOfBoundsException('The appropriate routing information not ready to execute');
+        }
+
+        $out = '';
+
+        foreach ( $this->actions as $action )
+        {
             $controllerClass = $action->getControllerClass();
             $method          = $action->getControllerMethod();
 
             $controller = new $controllerClass($this->request);
 
-            $out = $controller->$method($arguments);
+            $out = $controller->$method($this->arguments);
         }
 
         return $out;
